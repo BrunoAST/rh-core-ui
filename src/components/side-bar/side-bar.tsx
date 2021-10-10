1a98702934e7e44cccaa4eeb5c8c813d811fb2b7
Bruno Alves <bruno-alves2010@live.com>
2021-10-09 22:28:46 -0300

    feat: side bar component (WIP)

--- src/components/side-bar/side-bar.tsx
+++ src/components/side-bar/side-bar.tsx
@@ -1,14 +1,31 @@
-import { Component, h } from "@stencil/core";
+import { Component, getAssetPath, h, Prop } from "@stencil/core";
+import { Items } from "../../types/items";
 
 @Component({
   tag: "rh-side-bar",
   styleUrl: "./side-bar.scss",
-  shadow: true
+  shadow: true,
+  assetsDirs: ["assets"]
 })
 export class SideBar {
+  @Prop({ attribute: "menu-items", mutable: true, reflect: true }) menuItems: Items[] = [];
+
   render() {
     return [
-      <h1>Hello</h1>
+      <aside class="container">
+        <header class="header">
+          <h1 class="header__title">Logo</h1>
+          <div class="header__menu">
+            <img src={getAssetPath("../../assets/menu.svg")} alt="Menu" />
+          </div>
+        </header>
+
+        <rh-side-bar-items menuItems={this.menuItems} />
+
+        <footer>
+          Login/Logout
+        </footer>
+      </aside>
     ];
   }
 }
