7e4c3e4c42b4eb653d741e90f9842db5b2c5ce09
Bruno Alves <bruno-alves2010@live.com>
2021-10-27 09:27:19 -0300

    feat: add waitForChanges in componentSetup

--- src/utils/component-setup/component-setup.ts
+++ src/utils/component-setup/component-setup.ts
@@ -2,15 +2,17 @@ import { newSpecPage } from "@stencil/core/testing";
 
 type ComponentSetupType = {
   shadowRoot: ShadowRoot;
+  waitForChanges: () => Promise<any>;
 }
 
 const componentSetup = async (templateBody: any, ...component: any): Promise<ComponentSetupType> => {
-  const { root } = await newSpecPage({
+  const { root, waitForChanges } = await newSpecPage({
     components: [...component],
     template: () => templateBody
   });
   return {
-    shadowRoot: root.shadowRoot
+    shadowRoot: root.shadowRoot,
+    waitForChanges
   }
 }
 
