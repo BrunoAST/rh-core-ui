a9bd28a087311a4028dbfacf49ba321795e89229
Bruno Alves <bruno-alves2010@live.com>
2021-11-14 21:20:03 -0300

    docs: add input stories

---
+++ src/stories/Input.stories.ts
@@ -0,0 +1,35 @@
+import { Meta } from "@storybook/html";
+import "ionicons";
+
+export default {
+  title: "Componentes/Inputs/Input",
+  parameters: {
+    layout: "centered"
+  },
+  argTypes: {
+    isInvalid: {
+      control: { type: "boolean" },
+      description: "Define os estado de validade do input",
+      table: {
+        defaultValue: {
+          summary: "false"
+        }
+      }
+    }
+  },
+  args: {
+    label: "E-mail",
+    type: "text",
+    isInvalid: false
+  }
+} as Meta;
+
+const Template = ({ label, type, isInvalid }) => {
+  const input = document.createElement("rh-input");
+  input.label = label;
+  input.type = type;
+  input.isInvalid = isInvalid;
+  return input;
+}
+
+export const Input = Template.bind({});
