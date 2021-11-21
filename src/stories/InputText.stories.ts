4be69406b76fe471807eb3915e2e7e6c9d497cda
Bruno Alves <bruno-alves2010@live.com>
2021-11-21 09:15:31 -0300

    docs: add base docs for inputs

--- src/stories/InputText.stories.ts
+++ src/stories/InputText.stories.ts
@@ -1,14 +1,11 @@
 import { Meta } from "@storybook/html";
 import "ionicons";
+import { inputsBaseArgs, inputsBaseArgTypes, inputsBaseParameters } from "./shared/input-base-args";
 
 export default {
   title: "Componentes/Inputs/Input Text",
   parameters: {
-    layout: "centered",
-    actions: {
-      handles: ["value"]
-    },
-    docs: { inlineStories: false }
+    ...inputsBaseParameters
   },
   argTypes: {
     type: {
@@ -21,77 +18,11 @@ export default {
         }
       }
     },
-    name: {
-      control: { type: "text" },
-      description: "Nome do form control submetido com o formulário como parte do conjunto `name/value`",
-      table: {
-        defaultValue: {
-          summary: ""
-        }
-      }
-    },
-    placeholder: {
-      control: { type: "text" },
-      description: "Texto que aparece no input quando nenhum valor foi inserido",
-      table: {
-        defaultValue: {
-          summary: ""
-        }
-      }
-    },
-    label: {
-      control: { type: "text" },
-      description: "Provém uma explicação sobre do que se trata o input",
-      table: {
-        defaultValue: {
-          summary: ""
-        }
-      }
-    },
-    ariaLabel: {
-      control: { type: "text" },
-      description: "Descreve o input. Utilizado por ferramentas de acessibilidade",
-      table: {
-        defaultValue: {
-          summary: ""
-        }
-      }
-    },
-    isInvalid: {
-      control: { type: "boolean" },
-      description: "Define os estado de validade do input",
-      table: {
-        defaultValue: {
-          summary: "false"
-        }
-      }
-    },
-    isRequired: {
-      control: { type: "boolean" },
-      description: "Define os estado de obrigatoriedade do input",
-      table: {
-        defaultValue: {
-          summary: "false"
-        }
-      }
-    },
-    value: {
-      description: "Evento emitido sempre que o valor do input é alterado. O valor em string é emitido na propriedade `detail` do evento",
-      table: {
-        defaultValue: {
-          summary: "CustomEvent<string>"
-        }
-      }
-    }
+    ...inputsBaseArgTypes
   },
   args: {
-    label: "Nome",
     type: "text",
-    name: "",
-    placeholder: "",
-    ariaLabel: "",
-    isInvalid: false,
-    isRequired: false
+    ...inputsBaseArgs
   }
 } as Meta;
 
---
+++ src/stories/shared/input-base-args.ts
@@ -0,0 +1,81 @@
+export const inputsBaseArgTypes = {
+  name: {
+    control: { type: "text" },
+    description: "Nome do form control submetido com o formulário como parte do conjunto `name/value`",
+    table: {
+      defaultValue: {
+        summary: ""
+      }
+    }
+  },
+  placeholder: {
+    control: { type: "text" },
+    description: "Texto que aparece no input quando nenhum valor foi inserido",
+    table: {
+      defaultValue: {
+        summary: ""
+      }
+    }
+  },
+  label: {
+    control: { type: "text" },
+    description: "Provém uma explicação sobre do que se trata o input",
+    table: {
+      defaultValue: {
+        summary: ""
+      }
+    }
+  },
+  ariaLabel: {
+    control: { type: "text" },
+    description: "Descreve o input. Utilizado por ferramentas de acessibilidade",
+    table: {
+      defaultValue: {
+        summary: ""
+      }
+    }
+  },
+  isInvalid: {
+    control: { type: "boolean" },
+    description: "Define os estado de validade do input",
+    table: {
+      defaultValue: {
+        summary: "false"
+      }
+    }
+  },
+  isRequired: {
+    control: { type: "boolean" },
+    description: "Define os estado de obrigatoriedade do input",
+    table: {
+      defaultValue: {
+        summary: "false"
+      }
+    }
+  },
+  value: {
+    description: "Evento emitido sempre que o valor do input é alterado. O valor em string é emitido na propriedade `detail` do evento",
+    table: {
+      defaultValue: {
+        summary: "CustomEvent<string>"
+      }
+    }
+  }
+}
+
+export const inputsBaseArgs = {
+  label: "Nome",
+  name: "",
+  placeholder: "",
+  ariaLabel: "",
+  isInvalid: false,
+  isRequired: false
+}
+
+export const inputsBaseParameters = {
+  layout: "centered",
+  actions: {
+    handles: ["value"]
+  },
+  docs: { inlineStories: false }
+}
