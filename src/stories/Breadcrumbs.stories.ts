6e150bb8b2d03d2d838b6f52fde51379f1765c0a
Bruno Alves <bruno-alves2010@live.com>
2021-11-21 14:26:42 -0300

    docs: add breadcrumbs stories

---
+++ src/stories/Breadcrumbs.stories.ts
@@ -0,0 +1,52 @@
+import { Meta } from "@storybook/html";
+import "ionicons";
+import { BreadcrumbsPaths } from "../components/breadcrumbs/types/breadcrumbs-paths";
+
+export default {
+  title: "Componentes/Breadcrumbs",
+  parameters: {
+    layout: "centered",
+    actions: {
+      handles: ["pathClicked"],
+    },
+  },
+  argTypes: {
+    paths: {
+      control: { type: "object" },
+      description: "Lista com todos os caminhos referentes ao nível de uma página",
+      table: {
+        defaultValue: {
+          summary: "[]"
+        }
+      }
+    },
+    pathClicked: {
+      description: "Evento que emite a url da opção clicada. A url é emitida na propriedade `detail` do evento",
+      table: {
+        defaultValue: {
+          summary: "CustomEvent<string>"
+        }
+      }
+    }
+  },
+  args: {
+    paths: [
+      {
+        name: "Perguntas",
+        pageUrl: "/perguntas"
+      },
+      {
+        name: "Criar perguntas",
+        pageUrl: "/criar-perguntas"
+      }
+    ] as BreadcrumbsPaths[]
+  }
+} as Meta;
+
+const Template = ({ paths }) => {
+  const breadcrumbs = document.createElement("rh-breadcrumbs");
+  breadcrumbs.paths = paths;
+  return breadcrumbs;
+}
+
+export const Breadcrumbs = Template.bind({});
