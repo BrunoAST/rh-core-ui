fe9ba9aeafa6af22379152df19ab09f644f380eb
Bruno Alves <bruno-alves2010@live.com>
2022-01-07 09:53:16 -0300

    test: add test for window resize event

--- src/components/side-bar/test/side-bar.spec.tsx
+++ src/components/side-bar/test/side-bar.spec.tsx
@@ -1,4 +1,5 @@
 import { h } from "@stencil/core";
+import { dispatchWindowEvent } from "../../../shared/helpers/window-event";
 import componentSetup from "../../../utils/component-setup/component-setup";
 import { SideBarItems } from "../components/side-bar-items/side-bar-items";
 import { menuItems } from "../components/side-bar-items/test/mock-menu-items";
@@ -30,7 +31,7 @@ describe("SideBar component", () => {
   test("Should collapse side bar when click in backdrop", async () => {
     let isCollapsed: boolean;
     const { shadowRoot } = await componentSetup(
-      <rh-side-bar onIsCollapsed={(event) => isCollapsed = event.detail} />,
+      <rh-side-bar menuItems={menuItems} onIsCollapsed={(event) => isCollapsed = event.detail} />,
       SideBar
     );
     (shadowRoot.getElementById("header-button") as HTMLButtonElement).click();
@@ -47,5 +48,36 @@ describe("SideBar component", () => {
     expect(shadowRoot.querySelector("main").classList.contains("main-content__collapsed")).toBeFalsy();
     (shadowRoot.getElementById("header-button") as HTMLButtonElement).click();
     expect(shadowRoot.querySelector("main").classList.contains("main-content__collapsed")).toBeTruthy();
+  });
+
+  test("Should collapse when window innerWidth is less than 960 pixels wide", async () => {
+    let isCollapsed: boolean;
+    const { shadowRoot } = await componentSetup(
+      <rh-side-bar menuItems={menuItems} onIsCollapsed={(event) => isCollapsed = event.detail} />,
+      SideBar
+    );
+    window.innerWidth = 500;
+    dispatchWindowEvent("resize");
+    expect(shadowRoot.querySelector("main").classList.contains("main-content__collapsed")).toBeTruthy();
+    expect(shadowRoot.querySelector(".container").classList.contains("container__collapsed")).toBeTruthy();
+    expect(shadowRoot.querySelector(".backdrop").classList.contains("backdrop__active")).toBeFalsy();
+    expect(isCollapsed).toBeTruthy();
+  });
+
+  test("Should expand when window innerWidth is greater than 960 pixels wide", async () => {
+    let isCollapsed: boolean;
+    const { shadowRoot } = await componentSetup(
+      <rh-side-bar menuItems={menuItems} onIsCollapsed={(event) => isCollapsed = event.detail} />,
+      SideBar
+    );
+    window.innerWidth = 500;
+    dispatchWindowEvent("resize");
+    expect(shadowRoot.querySelector("main").classList.contains("main-content__collapsed")).toBeTruthy();
+    window.innerWidth = 1080;
+    dispatchWindowEvent("resize");
+    expect(shadowRoot.querySelector("main").classList.contains("main-content__collapsed")).toBeFalsy();
+    expect(shadowRoot.querySelector(".container").classList.contains("container__collapsed")).toBeFalsy();
+    expect(shadowRoot.querySelector(".backdrop").classList.contains("backdrop__active")).toBeFalsy();
+    expect(isCollapsed).toBeFalsy();
   });
 });
